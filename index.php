878f28b3986e39e059f4bd0f7753d249f8e9a7bb
Taylan Atreu da Silva <taylan@guiafacil.com.br>
2023-03-06 14:45:54 -0300

    criando o index do login

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
-    <title>Tiny Dashboard - A Bootstrap Dashboard Template</title>
+    <title>SEMAGRI - Secretaria Municipal de Agricultura</title>
     <!-- Simple bar CSS -->
     <link rel="stylesheet" href="css/simplebar.css">
     <!-- Fonts CSS -->
@@ -23,7 +23,7 @@
     <div class="wrapper vh-100">
       <div class="row align-items-center h-100">
         <form class="col-lg-3 col-md-4 col-10 mx-auto text-center">
-          <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="./index.html">
+          <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="./index.php">
             <svg version="1.1" id="logo" class="navbar-brand-img brand-md" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 120 120" xml:space="preserve">
               <g>
                 <polygon class="st0" points="78,105 15,105 24,87 87,87 	" />
@@ -32,21 +32,17 @@
               </g>
             </svg>
           </a>
-          <h1 class="h6 mb-3">Sign in</h1>
+          <h1 class="h6 mb-3">SEMAGRI</h1>
           <div class="form-group">
-            <label for="inputEmail" class="sr-only">Email address</label>
-            <input type="email" id="inputEmail" class="form-control form-control-lg" placeholder="Email address" required="" autofocus="">
+            <label for="inputEmail" class="sr-only">Login</label>
+            <input type="email" id="inputEmail" class="form-control form-control-lg" placeholder="Digite seu login" required="" autofocus="">
           </div>
           <div class="form-group">
-            <label for="inputPassword" class="sr-only">Password</label>
-            <input type="password" id="inputPassword" class="form-control form-control-lg" placeholder="Password" required="">
+            <label for="inputPassword" class="sr-only">Senha</label>
+            <input type="password" id="inputPassword" class="form-control form-control-lg" placeholder="Digite sua senha" required="">
           </div>
-          <div class="checkbox mb-3">
-            <label>
-              <input type="checkbox" value="remember-me"> Stay logged in </label>
-          </div>
-          <button class="btn btn-lg btn-primary btn-block" type="submit">Let me in</button>
-          <p class="mt-5 mb-3 text-muted">© <?=date("Y")?></p>
+          <button class="btn btn-lg btn-primary btn-block" type="submit">Logar</button>
+          <p class="mt-5 mb-3 text-muted">Secretaria Municipal de Agricultura <br /> Todos os direitos reservados © <?=date("Y")?></p>
         </form>
       </div>
     </div>
